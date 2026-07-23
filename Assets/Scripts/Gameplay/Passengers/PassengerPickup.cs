5384f1be0b82a929e0e33b5bc792d7551f67f60c
Tenseven-107 <nadrichem@tanous.nl>
2026-07-23 21:32:04 +0200

    Added pickup points dissapearing and reappearing

--- Assets/Scripts/Gameplay/Passengers/PassengerPickup.cs
+++ Assets/Scripts/Gameplay/Passengers/PassengerPickup.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class PassengerPickup : MonoBehaviour
 {
+    [SerializeField] private float minResetTime = 8f;
+    [SerializeField] private float maxResetTime = 15f;
+    [SerializeField] private float stayTime = 20f;
+
     [Header("Rendering")]
     [SerializeField] private SpriteRenderer passengerRenderer;
     [SerializeField] private SpriteRenderer zoneRenderer;
     [SerializeField] private SpriteRenderer outerZoneRenderer;
     [SerializeField] private PassengerColorSchemeCollection colors;
+
+    private bool _isActive;
+    private Coroutine _currentStayCoroutine;
 
     private PassengerColorScheme _currentPassengerColorScheme;
     private Collider2D _collider;
@@ -19,13 +26,47 @@ public class PassengerPickup : MonoBehaviour
         _collider = GetComponent<Collider2D>();
         _collider.isTrigger = true;
 
-        //passengerRenderer.sprite = null;
         Activate();
     }
 
     private void Activate()
     {
+        passengerRenderer.enabled = true;
+        zoneRenderer.enabled = true;
+        outerZoneRenderer.enabled = true;
+
+        _collider.enabled = true;
+
         _currentPassengerColorScheme = colors.GetRandomColorScheme();
+        passengerRenderer.color = _currentPassengerColorScheme.BaseColor; // USE SHADER LATER
+
+        _isActive = true;
+
+        Debug.Log("Passenger " + gameObject.name + " appeared!");
+    }
+
+    private void OnBecameVisible() => StopStayTimer();
+
+    private void StopStayTimer()
+    {
+        if (_currentStayCoroutine == null) return;
+        StopCoroutine(_currentStayCoroutine);
+
+        Debug.Log("Stopping stay coroutine on passenger pickup: " + gameObject.name);
+    }
+
+    private void OnBecameInvisible()
+    {
+        if (_isActive == false) return;
+        _currentStayCoroutine = StartCoroutine(StayTimeCoroutine());
+
+        Debug.Log("Starting stay coroutine on passenger pickup: " + gameObject.name);
+    }
+
+    private IEnumerator StayTimeCoroutine()
+    {
+        yield return new WaitForSeconds(stayTime);
+        Deactivate();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -42,10 +83,27 @@ public class PassengerPickup : MonoBehaviour
         cabin.Pickup(newPassenger);
 
         Deactivate();
+        StopStayTimer();
     }
 
     private void Deactivate() // Deactivate zone and start timer to activate
     {
-        return;
+        passengerRenderer.enabled = false;
+        zoneRenderer.enabled = false;
+        outerZoneRenderer.enabled = false;
+
+        _collider.enabled = false;
+        _isActive = false;
+
+        StartCoroutine(ResetTimeCoroutine());
+
+        Debug.Log("Passenger " + gameObject.name + " disappeared");
+    }
+
+    private IEnumerator ResetTimeCoroutine()
+    {
+        var resetTime = Random.Range(minResetTime, maxResetTime);
+        yield return new WaitForSeconds(resetTime);
+        Activate();
     }
 }
